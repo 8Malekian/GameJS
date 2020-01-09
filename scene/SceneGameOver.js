242dbc9a92bcf126ae707dce1558d40acdafd3f4
Dijkstra <contiemathias@gmail.com>
2020-01-09 19:26:39 +0100

    correction des explosions et ajout du menu

--- scene/SceneGameOver.js
+++ scene/SceneGameOver.js
@@ -2,7 +2,16 @@ class SceneGameOver extends Phaser.Scene {
     constructor() {
       super({ key: "SceneGameOver"});
     }
+    preload(){
+      this.load.image("endTitle", "img/game-over-blue.jpg"); 
+      this.load.image('end', 'img/gameOverbck.png'); 
+
+  }
     create() {
-        
+      this.add.image(400, 300, 'end');  
+      this.start= this.add.sprite(
+        this.game.config.width * 0.5,
+        this.game.config.height * 0.5,
+        "endTitle" )
     }
   }
