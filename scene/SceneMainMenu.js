6146a156479e2341e0f6e9d49b791d8b5383d6af
Dijkstra <contiemathias@gmail.com>
2020-01-10 21:55:15 +0100

    ajout l'image du debut

--- scene/SceneMainMenu.js
+++ scene/SceneMainMenu.js
@@ -3,12 +3,34 @@ class SceneMainMenu extends Phaser.Scene {
       super({ key: "SceneMainMenu", active: true});
     }
     preload(){
-        this.load.image("btnPlay", "img/btndark.png");
-        this.load.image("btnPlayHover", "img/btnlight.png");
-        this.load.image("btnPlayDown", "img/btndarker.png");
+        this.load.image("btnPlay", "img/Start_On.png");
+        this.load.image("btnPlayDown", "img/Start_off.png");
+        this.load.image('start', 'img/start.jpg'); 
 
     }
     create() {
-      this.scene.start("SceneMain");
+      this.add.image(400, 170, 'start');  
+      this.start= this.add.sprite(
+        this.game.config.width * 0.5,
+        this.game.config.height * 0.85,
+        "btnPlay"
+      );
+      this.start.setInteractive();
+
+      this.start.on("pointerdown", function() {
+        this.start.setTexture("btnPlayDown");
+      }, this);
+
+      this.start.on("pointerup", function() {
+        this.start.setTexture("btnPlay");
+        this.scene.start("SceneMain");
+      }, this);
+
+      this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+      
+      this.space.on(this.space.isDown,function () {
+        this.scene.start("SceneMain");
+      },this)
+      
     }
 }  
