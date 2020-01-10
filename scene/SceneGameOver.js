b9812a605a6a0daf6a26ad9fe35e1203ffb022fa
Dijkstra <contiemathias@gmail.com>
2020-01-10 21:54:46 +0100

    ajout de la scene de fin

--- scene/SceneGameOver.js
+++ scene/SceneGameOver.js
@@ -2,16 +2,11 @@ class SceneGameOver extends Phaser.Scene {
     constructor() {
       super({ key: "SceneGameOver"});
     }
-    preload(){
-      this.load.image("endTitle", "img/game-over-blue.jpg"); 
-      this.load.image('end', 'img/gameOverbck.png'); 
+    preload(){       
+      this.load.image('end', 'img/gameOverbck.jpg'); 
 
   }
     create() {
       this.add.image(400, 300, 'end');  
-      this.start= this.add.sprite(
-        this.game.config.width * 0.5,
-        this.game.config.height * 0.5,
-        "endTitle" )
     }
   }
